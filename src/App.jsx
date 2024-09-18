841433998ad9098d90b1f68723003db6e5284a23
hg <88635129+hyperingenious@users.noreply.github.com>
2024-09-18 10:59:32 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 
-import { MantineProvider } from '@mantine/core';
+import {  MantineProvider } from "@mantine/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Import the pages you are using
+import CheckAllBreaches from "./pages/CheckAllBreaches";
 
 export default function App() {
-  return <MantineProvider></MantineProvider>;
+  return (
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/check-breaches" element={<CheckAllBreaches/>} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
 }
