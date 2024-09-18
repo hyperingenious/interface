b23d0a0789abe0cea9563797587fd18315e1eb81
ab7022 <bayees1@gmail.com>
2024-09-18 12:45:48 +0530

    added landing page

--- src/data/BreachAnalytics.jsx
+++ src/data/BreachAnalytics.jsx
@@ -1,42 +1,17 @@
-import React, { useState } from 'react';
 import axios from 'axios';
 
-export default function BreachAnalytics() {
-  const [email, setEmail] = useState('');
-  const [result, setResult] = useState(null);
+export default function BreachAnalytics(email) {
+ 
 
   async function fetchData(email) {
     try {
       const response = await axios.get(`https://api.xposedornot.com/v1/breach-analytics?email=${email}`);
-      setResult(response.data); // axios automatically parses JSON response
+      const data = response.data
+      return data;
     } catch (error) {
       console.error('Error fetching data:', error);
-      setResult("Not Found");
+      return error;
     }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchData(email);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-        />
-        <button type="submit">Check Email</button>
-      </form>
-      {result && (
-        <div>
-          <h3>Result:</h3>
-          <pre>{JSON.stringify(result, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  );
 }
